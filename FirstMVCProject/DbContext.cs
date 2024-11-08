b8c53613ded415ab981725d87dd7f267075e975f
Ayorinde Smart <ayorinde.smart@devseal.tech>
2024-11-08 15:59:33 +0100

    First Commit

---
+++ FirstMVCProject/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using FirstMVCProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FirstMVCProject
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        //configure inmemory database for entityframework
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MovieDB");
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ FirstMVCProject/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstMVCProject.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Genre { get; set; }
+    }
+}
